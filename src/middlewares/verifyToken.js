c896f0d0806b1ccaca49049cbfa9bc078adfdf4b
Facundo <facundolopezcrespo@hotmail.com>
2022-01-29 20:38:34 -0300

    Change to capital letter

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -4,7 +4,7 @@ function verifyToken (req,res,next) {
     const accessToken=req.headers['authorization']||req.query.accesToken
     jwt.verify(accessToken,"secreto",(error,user)=>{
         if(error) {
-        res.send("acceso denegado. Token incorrecto o experidado")
+        res.send("Acceso denegado. Token incorrecto o experidado")
         } else {
         next() 
         }
